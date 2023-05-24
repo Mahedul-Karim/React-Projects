0edc53c5d55453ce35f60ad63b3999139a51ff4a
Mahedul-Karim <125360313+Mahedul-Karim@users.noreply.github.com>
2023-05-24 21:37:46 +0600

    Delete Basic E-commerce directory

--- Basic E-commerce/src/components/utilities/Number.js
+++
@@ -1,9 +0,0 @@
-const numberSet = function (number) {
-  const price = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(number / 100);
-
-  return price
-};
-export default numberSet;
